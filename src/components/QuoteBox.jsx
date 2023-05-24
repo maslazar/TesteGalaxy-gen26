7335afb1fcc185a5136cd69713b017c5b29444a9
Manuela <maslazar1995@gmail.com>
2023-05-23 21:03:58 -0500

    alt

--- src/components/QuoteBox.jsx
+++ src/components/QuoteBox.jsx
@@ -7,7 +7,7 @@ const QuoteBox = ({phrase, handleChangeQuote}) => {
         <article className='quoteBox___phrase'>
           <p>{phrase}</p>
         </article>
-        <button className='quoteBox___btn' onClick={handleChangeQuote}><img src="/images/bg5.png" alt="reload" /></button>
+        <button className='quoteBox___btn' onClick={handleChangeQuote}><img src="/images/bg5.png" /></button>
       </section>
   )+
 }
